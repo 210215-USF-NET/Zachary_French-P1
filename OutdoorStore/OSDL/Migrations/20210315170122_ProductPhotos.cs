482b16ed706c278a1803f483cdb8edf54c5ff052
ZMFrench <inbox@zachfrench.com>
2021-03-15 13:16:09 -0400

    Added images to Products, rescaffolded the database

---
+++ OutdoorStore/OSDL/Migrations/20210315170122_ProductPhotos.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OSDL.Migrations
+{
+    public partial class ProductPhotos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Photo",
+                table: "Products",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Photo",
+                table: "Products");
+        }
+    }
+}
